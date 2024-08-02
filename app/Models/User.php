a3dae5dcfc78de205c69c08efedc2be28f2f4ad4
Ari Jehadun <arijehadun@gmail.com>
2024-08-02 23:31:47 +0700

    modifikasi test 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, 'labMetPenerbangan19@gmail.com');
+        return str_ends_with($this->email, '@gmail.com');
     }
 }
